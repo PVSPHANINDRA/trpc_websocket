ad10b047b4a3d8076792e421991e5cbcb3b7f570
Phanindra Pvs <vpamidimukka@umass.edu>
2023-08-14 19:12:17 +0530

    tested various websocket scenarios

--- src/client.ts
+++ src/client.ts
@@ -1,59 +1,104 @@
-import {createTRPCProxyClient, createWSClient, httpLink, splitLink, wsLink} from '@trpc/client'
-import type {AppRouter} from './server'
+import { createTRPCProxyClient, createWSClient, httpLink, splitLink, wsLink } from "@trpc/client";
+import type { AppRouter } from "../../../noteaid/server/routers/_app";
 
-import ws from 'ws';
+import ws from "ws";
 
 globalThis.WebSocket = ws as any;
 
 const wsClient = createWSClient({
-  url: `ws://localhost:2000`
-})
+  url: `ws://localhost:3001`,
+});
 
 // links and splitLink
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     splitLink({
-      condition(op){
-        return op.type === 'subscription'
+      condition(op) {
+        console.log(op);
+        return true;
       },
       true: wsLink({
-        client: wsClient
+        client: wsClient,
       }),
       false: httpLink({
-        url: 'http://localhost:2000'
-      })
-    })
-  ]
-})
+        url: "http://localhost:3001",
+      }),
+    }),
+  ],
+});
+
+// wsClient.getConnection().onmessage = (data) => {
+//   console.log("in the message", data);
+// };
+
+// wsClient.getConnection().onclose = () => {
+//   console.info('close')
+//   wsClient.close()
+// }
 
 
+async function main() {
+  console.log("connection done");
+  // const helloResponse = await trpc.hello.query().catch((err) => console.log('error', err));
+  // console.log("response from hello endpoint", helloResponse);
 
-async function main(){
-  const helloResponse = await trpc.hello.query()
-  console.log('response from hello endpoint', helloResponse)
- 
+  // for (let i = 0; i < 2; i++) {
+  //   // await new Promise((resolve) => setTimeout(() => resolve("hey"), 1000));
+  //   const helloResponse = await trpc.hello.query().catch((err) => console.log('error', err));
+  //   console.log("response from hello endpoint", helloResponse);
+  // }
+
   let counter = 0
-  await new Promise<void>((resolve) => {
-    const subId = trpc.event.subscribe(undefined, {
+  new Promise<void>(async (resolve) => {
+    const subId1 = trpc.event.subscribe(0, {
+      onStarted(){
+        console.log('susbcription started')
+      },
       onData(data) {
         console.log(data)
         counter++;
         console.log(counter)
-        if(counter > 3){
-          subId.unsubscribe();
+        if(counter > 7){
+          subId1.unsubscribe() // unsubscribing the event
           resolve();
         }
       },
       onError(err) {
-        console.error('error', err);
+        console.error('susbcription error', err);
       },
-    })
+      onComplete() {
+        console.log('susbcription ended')
+      }
+    },)
+
+    // const subId2 = trpc.event.subscribe(1, {
+    //   onStarted(){
+    //     console.log('connection started')
+    //   },
+    //   onData(data) {
+    //     console.log(data)
+    //     counter++;
+    //     console.log(counter)
+    //     subId1.unsubscribe()
+    //     if(counter > 7){
+    //       resolve();
+    //     }
+    //   },
+    //   onError(err) {
+    //     console.error('error', err);
+    //   },
+    //   onComplete() {
+    //     console.log('complete')
+    //   }
+    // },)
+
+    // // Test: Cancelling the subscription triggered event before completion.
+    // // If the event callback is called on the server, it completes the functionality, except the client does not receive any data
+    // await new Promise((resolve) => setTimeout(() => {resolve('hi')}, 2000))
+    // subId1.unsubscribe()
   })
 
-  wsClient.close()
-
+  // wsClient.close()
 }
 
-await main()
-
-console.log('done')
+await main();
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import {inferAsyncReturnType, initTRPC} from '@trpc/server'
+import {TRPCError, inferAsyncReturnType, initTRPC} from '@trpc/server'
 import {createHTTPServer, CreateHTTPContextOptions} from '@trpc/server/adapters/standalone'
 import {WebSocketServer} from 'ws'
 import {CreateWSSContextFnOptions, applyWSSHandler} from '@trpc/server/adapters/ws'
 
 import { observable } from '@trpc/server/observable'
-
+import {z} from 'zod';
 
 // handling context
 function createContext(opts:CreateHTTPContextOptions | CreateWSSContextFnOptions){
@@ -13,27 +13,65 @@ function createContext(opts:CreateHTTPContextOptions | CreateWSSContextFnOptions
 
 type Context = inferAsyncReturnType<typeof createContext>
 
+let x = {'a': 1, 'b': 2} // TEST: how global variable is updating
 const t = initTRPC.context<Context>().create()
 
 const procedure = t.procedure;
 const router = t.router;
 
 const appRouter = router({
-  hello: procedure.query(() => {
-    return 'Hello'
+  hello: procedure.query((opts) => {
+    console.log('hello query')
+    // // Test: when error is thrown, does the websocket still on. Answer: the websocket is stil on. Even we throw some error.
+    // throw new TRPCError({
+    //   code: 'UNAUTHORIZED',
+    //   message: 'You are not authorized to send messages to this session',
+    // });
+    // // return 'hello'
   }),
-  event: procedure.subscription(() =>{
+  event: procedure.input(z.number()).subscription((opts) =>{
+    const input = opts.input
+    // // Test:
+    // // from the above it is clear that, even though we throw error, the websocket connection still persists. What about error on the initial sub req?
+    // // Answer: any error during the initial subscription request, will not start the subscription. 
+    // if(!input){
+    //   throw new TRPCError({
+    //     code: 'FORBIDDEN',
+    //     message:'he'
+    //   })
+    // }
     // return the observable -> this creates an observable stream that will emit data to the subscribed clients
-    return observable<{count: number}>((emit) => {
-        const intervalId = setInterval(() => {
-          // emit is the method used to send the data
-          emit.next({count: Math.random()})
-        }, 1000)
-
-        // return the function. This is will be triggered when the client unsubscribes
-        return () => {
-          clearInterval(intervalId)
+    return observable<{count: number, x: number}, {code: string, message: 'hgey'}>((emit) => {
+ 
+      const intervalId = setInterval(async () => {
+        // emit is the method used to send the data
+        const random = Math.random()
+        if(input){
+          x['a'] +=1
         }
+        else{
+          x['a'] -=1
+        }
+        // // Test:
+        // // from the above it is clear that, even though we throw error, the websocket connection still persists. What about subscription?
+        // // Answer: Any error after subsctiption starts, subscription will be closed. NOTE: any error during the initial subscription request, will not start the subscription. 
+        // if(!input){
+        //   throw new TRPCError({
+        //     code: 'FORBIDDEN',
+        //     message:'he'
+        //   })
+        // }
+        // await new Promise((resolve) => setTimeout(() => {resolve('hi')}, 5000)) // TEST: client unsubscribe before completion of this callback function
+        const data = {count: random, x: input}
+        console.log(input, data)
+        emit.next(data)
+        // console.log('data send to the client')
+      }, 1000)
+      // return the function. This is will be triggered when the client unsubscribes
+      return () => {
+        console.info('subscriptuon ended')
+        clearInterval(intervalId)
+      }
     })
   })
 })
@@ -43,7 +81,7 @@ export type AppRouter = typeof appRouter;
 // create http server
 const {server, listen} = createHTTPServer({
   router: appRouter,
-  createContext
+  createContext,
 })
 
 // create websocket server object
@@ -51,10 +89,42 @@ const wss = new WebSocketServer({
   server
 })
 
-applyWSSHandler<AppRouter>({
+const handler= applyWSSHandler<AppRouter>({
   wss,
-  router: appRouter,
-  createContext
+  router: appRouter
 })
 
-listen(2000)
+wss.on('connection', (ws, req) => {
+  ws['x-header1'] = 'header_1' // adding custom header 
+  ws.ping('hey') // pinging the client. and checking onpong is triggered or not
+  console.log('in the connection')
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.on('pong', () => {
+    console.log('pong')
+  })
+  ws.once('close', () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
+  // TEST: termianting the websocket, does client stop connecting to the server?
+  // Ans: Since subscriptions are meant to reconnect, if the client side, does not close the websocket from their end. The subscription event, tries to reconnect.
+  // To avoid this scenario, add the websocket close functionality on the client side. 
+  // setTimeout(() => ws.terminate(), 2000) // does terminating the client closing the subscription or not
+});
+
+wss.on('headers', (ws, header, req)=> {
+  console.log('in the headers')
+  // To see the follow of event in websocket. --> events --> headers, connection
+  // throw new TRPCError({
+  //     code: 'FORBIDDEN',
+  //     message:'he'
+  //   })
+})
+
+console.log('✅ WebSocket Server listening on ws://localhost:2000');
+process.on('SIGTERM', () => {
+  console.log('SIGTERM');
+  handler.broadcastReconnectNotification();
+  wss.close();
+});
+
+listen(3001)
